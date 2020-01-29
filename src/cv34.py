b983c56f470ddeb5ee7d26d84761255f2019b179
FaisalNajibAbdullah <faisalnajib23@gmail.com>
2020-01-29 14:22:10 +0700

    Najibbbbbbbbbb 150

---
+++ src/cv34.py
@@ -0,0 +1,21 @@
+import cv2
+
+import numpy as np
+
+def nothing(x):
+    pass
+
+cv2.namedWindow("Tracking")
+
+while True:
+    frame = cv2.imread('PARKIR.png')
+
+    cv2.imshow("frame", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
+
